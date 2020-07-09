ec3a2d3f876152acec012ff3a12ca9d54a976c31
aomsw13 <disneybabyaom@hotmail.com>
2020-07-09 23:34:49 +0700

    Create MyAdapter.kt
    create this file to do recyclerview and also make list item in cyclerview clickable then intent to GiverSelectPantryActivity.kt

---
+++ smartpantry/app/src/main/java/com/example/smartpantry/Adapter/MyAdapter.kt
@@ -0,0 +1,93 @@
+package com.example.smartpantry.Adapter
+
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smartpantry.GiverSelectPantryActivity
+import com.example.smartpantry.Model.PantryEmpty
+import com.example.smartpantry.R
+import helpers.MqttClient
+import helpers.MqttGiver
+
+
+class MyAdapter(internal var context: MutableList<PantryEmpty>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){ //, View.OnClickListener
+            internal var txt_pantryID: TextView
+            internal var txt_pantryStatus: TextView
+
+
+            init{
+                txt_pantryID = itemView.findViewById<TextView>(R.id.pantry_id_description)
+                txt_pantryStatus = itemView.findViewById<TextView>(R.id.pantry_status_description)
+
+//                itemView.setOnClickListener({
+//                    val mIntent = Intent(itemView.context, GiverSelectPantryActivity::class.java)
+//                    mIntent.putExtra("pantryIdAdapter", txt_pantryID.toString())
+//                    itemView.context.startActivity(mIntent)
+//                    Log.d("MyAdapter", "enter setOnclickListener & pantry ID is $txt_pantryID.toString()")
+//
+//                })
+            }
+
+//        override fun onClick(p0: View?) {
+//            val mIntent = Intent(itemView.context, GiverSelectPantryActivity::class.java)
+//            mIntent.putExtra("pantryIdAdapter", txt_pantryID.toString() )
+//            itemView.context.startActivity(mIntent)
+//            Log.d("MyAdapter", "enter setOnclickListener & pantry ID is $txt_pantryID.toString()")
+//
+//        }
+    }
+
+    internal  var userList: List<PantryEmpty>
+
+    val mqttClient: MqttGiver by lazy {
+        MqttGiver(this)
+
+    }
+
+
+    init{
+        this.userList = ArrayList()
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_layout,parent,false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return context.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.txt_pantryID.text = context[position].pantryID
+
+        holder.txt_pantryStatus.text = context[position].status
+        Log.d("MyAdapter", "enter setOnclickListener $context[position].pantryID")
+
+        holder.itemView.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(it: View?) {
+                val mIntent = Intent(it!!.context, GiverSelectPantryActivity::class.java)
+                mIntent.putExtra("pantryIdAdapter", context.get(position).pantryID )
+                it.context.startActivity(mIntent)
+                Log.d("MyAdapter", "enter setOnclickListener & pantry ID is " + context.get(position).pantryID)
+
+
+            }
+            
+
+
+        })
+    }
+
+
+}
+
