54fd98fc25fe9357a11c65ba2b874ee931627257
aomsw13 <disneybabyaom@hotmail.com>
2020-07-12 19:50:48 +0700

    Update UserType.kt
    load empty pantry status from cloudMQTT(MqttGiver.kt) before givers can see the page that show list of empty pantry

--- smartpantry/app/src/main/java/com/example/smartpantry/UserType.kt
+++ smartpantry/app/src/main/java/com/example/smartpantry/UserType.kt
@@ -2,15 +2,23 @@ package com.example.smartpantry
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import helpers.MqttGiver
 
 class UserType : AppCompatActivity() {
 
     private lateinit var takerButton: Button
     private lateinit var giverButton: Button
+
+    val mqttGiver: MqttGiver by lazy {
+        MqttGiver(this)
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +38,20 @@ class UserType : AppCompatActivity() {
             val mIntent = Intent(this, MainActivityGiver::class.java)
 //            mIntent.putExtra("userID", "0")
             startActivity(mIntent)
+            startMqtt()
+
 
         }
     }
+
+    private fun startMqtt() {
+        mqttGiver.connect(this.applicationContext)
+        mqttGiver.subscriptionTopicPantryId = "pantry/+/emptyPantryId"
+        mqttGiver.subscriptionTopicPantryStatus = "pantry/+/emptyPantryStatus"
+        mqttGiver.publishTextMessage = "open"
+        Log.d("FETCHING", "startMqtt")
+        //setValueToFirebase(mqttGiver.receiveTopicPantryId.toString(), mqttGiver.receiveTopicPantryStatus)
+    }
+
+
 }
