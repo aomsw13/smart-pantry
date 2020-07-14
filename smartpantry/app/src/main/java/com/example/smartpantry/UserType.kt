7a61afff2d0a91ee8fcf0e16ab8b6370d292c750
aomsw13 <disneybabyaom@hotmail.com>
2020-07-14 19:18:09 +0700

    Update UserType.kt

--- smartpantry/app/src/main/java/com/example/smartpantry/UserType.kt
+++ smartpantry/app/src/main/java/com/example/smartpantry/UserType.kt
@@ -28,6 +28,8 @@ class UserType : AppCompatActivity() {
         takerButton = findViewById(R.id.taker_button)
         giverButton = findViewById(R.id.giver_button)
 
+
+
         takerButton.setOnClickListener { view: View? ->
             val mIntent = Intent(this, MainActivity::class.java)
            // mIntent.putExtra("userID", "1")
@@ -51,6 +53,22 @@ class UserType : AppCompatActivity() {
         mqttGiver.publishTextMessage = "open"
         Log.d("FETCHING", "startMqtt")
         //setValueToFirebase(mqttGiver.receiveTopicPantryId.toString(), mqttGiver.receiveTopicPantryStatus)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
