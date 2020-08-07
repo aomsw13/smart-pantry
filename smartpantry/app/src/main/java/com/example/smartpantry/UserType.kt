d859c1116a4a88c4a2c3f766f5176b2c25f0e703
aomsw13 <disneybabyaom@hotmail.com>
2020-08-07 10:15:01 +0700

    Update UserType.kt
    add source code comment

--- smartpantry/app/src/main/java/com/example/smartpantry/UserType.kt
+++ smartpantry/app/src/main/java/com/example/smartpantry/UserType.kt
@@ -2,47 +2,38 @@ package com.example.smartpantry
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import helpers.MqttGiver
 
 class UserType : AppCompatActivity() {
 
     private lateinit var takerButton: Button
     private lateinit var giverButton: Button
 
-//    val mqttGiver: MqttGiver by lazy {
-//        MqttGiver(this)
-//
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usertype)
 
-
         takerButton = findViewById(R.id.taker_button)
         giverButton = findViewById(R.id.giver_button)
 
-
-
+        // a user when user click taker button and navigate a user to MainActivity
         takerButton.setOnClickListener { view: View? ->
             val mIntent = Intent(this, MainActivity::class.java)
-           // mIntent.putExtra("userID", "1")
+            //mIntent.putExtra("userID", "1")
             startActivity(mIntent)
         }
 
+        // a user when user click giver button and navigate a user to MainActivity
         giverButton.setOnClickListener { view: View? ->
             val mIntent = Intent(this, MainActivityGiver::class.java)
-//            mIntent.putExtra("userID", "0")
+            //mIntent.putExtra("userID", "0")
             startActivity(mIntent)
-           // startMqtt()
-
-
+            // startMqtt()
         }
     }
 
@@ -56,6 +47,8 @@ class UserType : AppCompatActivity() {
 //        //setValueToFirebase(mqttGiver.receiveTopicPantryId.toString(), mqttGiver.receiveTopicPantryStatus)
 //    }
 
+
+    // two below functions, OncreateOptionsMenu & onOptionsItemSelected, will not use in android mobile application
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
